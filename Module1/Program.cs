f8ea45f0ea03ad48658c9ce8674cf81e66b6437b
Scott Dyrness <scottdyrness@gmail.com>
2025-09-24 16:24:36 -0700

    Created Method AreaOfCircle

---
+++ Module1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Module1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        static double AreaOfCircle(double radius)
+        {
+            return Math.PI*radius*radius;
+        }
+        
+    }
+}
